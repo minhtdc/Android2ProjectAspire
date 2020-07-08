4a7f9f393f30f46ee68b0f2a886198e76676196d
minhtdc <67999471+minhtdc@users.noreply.github.com>
2020-07-08 15:27:58 +0700

    First commit

--- app/src/main/java/com/example/aspire/MainActivity.java
+++ app/src/main/java/com/example/aspire/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//  asdbfosadbfoasbdofbasudbfiausdbfiaubdsiufbasdiufbasiudbiuasbdfiubasdiubfiusda
         for (int i = 0; i < 100; i++) {
             break;
         }
