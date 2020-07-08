f4dc3619cf858caddc21bda171c6a87738592821
minhtdc <67999471+minhtdc@users.noreply.github.com>
2020-07-08 15:33:00 +0700

    alooooo

--- app/src/main/java/com/example/aspire/MainActivity.java
+++ app/src/main/java/com/example/aspire/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//  asdbfosadbfoasbdofbasudbfiausdbfiaubdsiufbasdiufbasiudbiuasbdfiubasdiubfiusdaadasd
         for (int i = 0; i < 100; i++) {
             break;
         }
